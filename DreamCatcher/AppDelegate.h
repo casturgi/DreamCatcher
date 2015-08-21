8f08c489af23041b840860a30cf7a49f4af7c7e1
Cory Sturgis-Casturgi <casturgi@gmail.com>
2015-08-20 19:26:34 -0500

    Initial Commit

---
+++ DreamCatcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DreamCatcher
+//
+//  Created by cory Sturgis on 8/20/15.
+//  Copyright (c) 2015 CorySturgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
